950bf05639d2d536b2ea6de08746c8354f33d700
Oche Ejeh <oeejeh@unionbankng.com>
2022-01-30 13:30:38 +0100

    add TDD to cover

--- src/test/java/com/ocheejeh/datastructures/MinimumElementTest.java
+++ src/test/java/com/ocheejeh/datastructures/MinimumElementTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MinimumElementTest {
 
     @Test
-    void shouldFindTheMinimumElement() {
+   public void shouldFindTheMinimumElement() {
         int[] test1 = {2, 3, 4, 1, 10, 12};
         int expect1 = 1;
         int actual1 = MinimumElement.findMin(test1);
@@ -18,5 +18,14 @@ class MinimumElementTest {
 
         assertEquals(expect1, actual1, "Should return the min element in the array");
         assertEquals(expect2, actual2, "Should return the min element in the array");
+    }
+
+    @Test
+    public void shouldReverseArrayElement(){
+        int[] test1 = {1, 2, 3, 4, 5};
+        int[] expect1 = {5, 4, 3, 2, 1};
+        MinimumElement.reverse(test1);
+        assertArrayEquals(expect1, test1, "should reverse all element in the array");
+
     }
 }
